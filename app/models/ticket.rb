629b5d3cced257cd00dff1356d1bfa36a367c966
saniyaa7 <saniyachaudhari07@gmail.com>
2024-02-10 12:21:40 +0530

    add column in ticket and movie show

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,4 +4,14 @@ class Ticket < ApplicationRecord
   validates :seat_no, :price, presence: true
   belongs_to :user
   belongs_to :movie_show
+  validates :payment_mode, inclusion: { in: %w(Online Cash),
+  message: "%{value} is not a valid payment_mode" }
+  before_validation :normalize
+  private
+
+  def normalize
+    self.payment_mode = payment_mode.downcase.titleize
+    
+  end
+  
 end
