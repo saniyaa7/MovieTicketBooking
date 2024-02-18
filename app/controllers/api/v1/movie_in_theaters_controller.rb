f9c2a8be7fc6b31a7d11e3d722b4114cd2cc4fae
saniyaa7 <114761701+saniyaa7@users.noreply.github.com>
2024-02-18 12:02:54 +0530

    Update movie_in_theaters_controller.rb

--- app/controllers/api/v1/movie_in_theaters_controller.rb
+++ app/controllers/api/v1/movie_in_theaters_controller.rb
@@ -3,12 +3,11 @@
 module Api
   module V1
     class MovieInTheatersController < ApplicationController
-      before_action :set_group, only: %i[show update destroy]
+      before_action :set_movie_in_theater, only: %i[show update destroy]
 
       def index
         authorize! :index, MovieInTheater
         @movie_in_theaters = MovieInTheater.all
-
         render json: @movie_in_theaters
       end
 
@@ -18,7 +17,7 @@ module Api
       end
 
       def create
-        @movie_in_theater = MovieInTheater.new(group_params)
+        @movie_in_theater = MovieInTheater.new(movie_in_theater_params)
         authorize! :create, @movie_in_theater
         if @movie_in_theater.save
           render json: @movie_in_theater, status: :created, location: api_v1_movie_in_theater(@movie_in_theater)
@@ -29,7 +28,7 @@ module Api
 
       def update
         authorize! :update, @movie_in_theater
-        if @movie_in_theater.update(group_params)
+        if @movie_in_theater.update(movie_in_theater_params)
           render json: @movie_in_theater
         else
           render json: @movie_in_theater.errors, status: :unprocessable_entity
@@ -47,16 +46,13 @@ module Api
 
       private
 
-      # Use callbacks to share common setup or constraints between actions.
-      def set_group
+      def set_movie_in_theater
         @movie_in_theater = MovieInTheater.find(params[:id])
         return if @movie_in_theater
-
         render json: { data: 'Record not found', status: 'failed' }
       end
 
-      # Only allow a list of trusted parameters through.
-      def group_params
+      def movie_in_theater_params
         params.require(:movie_in_theater).permit(:movie_show_id, :theater_id)
       end
     end
