eec29dfd7987b40b1ad1d4ccd02ec175be3dcd49
saniyaa7 <114761701+saniyaa7@users.noreply.github.com>
2024-02-18 11:59:18 +0530

    Update tickets_controller.rb

--- app/controllers/api/v1/tickets_controller.rb
+++ app/controllers/api/v1/tickets_controller.rb
@@ -3,12 +3,11 @@
 module Api
   module V1
     class TicketsController < ApplicationController
-      before_action :set_group, only: %i[show update destroy]
+      before_action :set_ticket, only: %i[show update destroy]
       # load_and_authorize_resource
       def index
         @tickets = Ticket.all
         authorize! :index, @ticket
-
         render json: @tickets
       end
 
@@ -18,10 +17,10 @@ module Api
       end
 
       def create
-        # @ticket = Ticket.new(group_params)
+        # @ticket = Ticket.new(ticket_params)
         # binding.pry
 
-        @ticket = current_user.tickets.new(group_params)
+        @ticket = current_user.tickets.new(ticket_params)
         authorize! :create, @ticket
         movie_show = MovieShow.find(@ticket.movie_show_id)
 
@@ -35,7 +34,7 @@ module Api
 
       def update
         authorize! :update, @ticket
-        if @ticket.update(group_params)
+        if @ticket.update(ticket_params)
           render json: @ticket
         else
           render json: @ticket.errors, status: :unprocessable_entity
@@ -44,7 +43,6 @@ module Api
 
       def destroy
         authorize! :destroy, @ticket
-
         if @ticket.destroy
           render json: { data: 'Ticket deleted successfully', status: 'success' }
         else
@@ -54,8 +52,7 @@ module Api
 
       private
 
-      # Use callbacks to share common setup or constraints between actions.
-      def set_group
+      def set_ticket
         @ticket = Ticket.find_by(id: params[:id])
 
         return if @ticket
@@ -63,8 +60,7 @@ module Api
         render json: { data: 'Ticket not found', status: 'failed' }
       end
 
-      # Only allow a list of trusted parameters through.
-      def group_params
+      def ticket_params
         params.require(:ticket).permit(:payment_mode, :seat_book, :user_id, :movie_show_id,
                                        seat_type: [])
       end
