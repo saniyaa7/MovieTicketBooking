63901159727e1d8cc21a677dcbe37a0036b7c7c2
saniyaa7 <saniyachaudhari07@gmail.com>
2024-03-26 12:47:35 +0530

    allow all get ability to user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,9 +9,14 @@ class Ability
       can :manage, :all
     else
       can :index, Movie
+      can :show, Movie
+      can :show, MovieShow
       can :index, MovieInTheater
-      can :set_movie, Movie # Fix syntax for set_movies
-      can :set_theater, Theater # Fix syntax for set_theaters
+      can :show, MovieInTheater
+      can :set_movie, Movie # Fix syntax for set_movie
+      can :set_theater, Theater # Fix syntax for set_theater
+      can :show, Theater
+      can :show, Ticket
       can :read, Movie
       can :index, MovieShow # Grant :index ability for MovieShow
       can :index, Theater
